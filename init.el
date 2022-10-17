48089638e001ad542c1e437af4653d664c8da614
Sachin <isachinsc2008@gmail.com>
2022-10-17 10:33:24 +0530

    Good theme

--- init.el
+++ init.el
@@ -1,9 +1,11 @@
-(package-initialize)
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://stable.melpa.org/packages/"))
-
+(setq user-emacs-directory "/home/sachin/.config/emacs")
 (require 'package)
 (require 'use-package)
+
+(package-initialize)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("stable-melpa" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
 (use-package evil
              :ensure t)
@@ -13,22 +15,22 @@
 (use-package org
              :ensure t)
 
-(use-package gruvbox-theme
-             :ensure t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#3c3836" "#fb4933" "#b8bb26" "#fabd2f" "#83a598" "#d3869b" "#8ec07c" "#ebdbb2"])
- '(custom-enabled-themes '(gruvbox))
- '(custom-safe-themes
-   '("1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" default))
- '(display-line-numbers-type 'relative)
- '(global-display-line-numbers-mode t)
- '(package-selected-packages '(evil-org org-bullets dashboard use-package))
- '(pdf-view-midnight-colors '("#fdf4c1" . "#1d2021")))
+;; (use-package gruvbox-theme
+;;              :ensure t)
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(ansi-color-names-vector
+;;    ["#3c3836" "#fb4933" "#b8bb26" "#fabd2f" "#83a598" "#d3869b" "#8ec07c" "#ebdbb2"])
+;;  '(custom-enabled-themes '(gruvbox))
+;;  '(custom-safe-themes
+;;    '("1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" default))
+;;  '(display-line-numbers-type 'relative)
+;;  '(global-display-line-numbers-mode t)
+;;  '(package-selected-packages '(doom-modeline use-package))
+;;  '(pdf-view-midnight-colors '("#fdf4c1" . "#1d2021")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -36,14 +38,14 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "JetBrains Mono" :foundry "JB" :slant normal :weight normal :height 158 :width normal)))))
 (setq display-line-numbers-type 'relative)
-(global-display-line-numbers-mode)
+(global-display-line-numbers-mode 1)
 
 (use-package dashboard
   :ensure t
   :config
   (dashboard-setup-startup-hook)
-  (setq dashboard-startup-banner "~/.emacs.d/goof.png")
-  (setq dashborad-banner-logo-titile "A kid who like to code"))
+  (setq dashboard-startup-banner "~/.config/emacs/goof.png")
+  (setq dashboard-banner-logo-title "A kid who like to code"))
 
 ;; Pair completion
 (electric-pair-mode 1)
@@ -63,10 +65,6 @@
 (use-package org-bullets
   :ensure t
   :after (evil org))
-(use-package org-superstar
-  :ensure t
-  :config
-  :(add-hook 'org-mode-hook (lambda () (org-superstart-mode 1))))
 
 (use-package evil-org
   :ensure t
@@ -80,3 +78,58 @@
                                         textobjects
                                         additional
                                         calendar)))))
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-gruvbox") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(exwm vterm doom-themes use-package org-bullets gruvbox-theme evil-org doom-modeline dashboard)))
+
+(use-package vterm
+  :ensure t
+  :config
+  (setq vterm-timer-delay 0.01))
+
+;; For exwm
+;; (require 'exwm)
+;; (require 'exwm-config)
+;; (exwm-config-default)
+
+;; This region is for transparency if you want
+;; (set-frame-parameter (selected-frame) 'alpha '(90 . 90))
+;; (add-to-list 'default-frame-alist '(alpha . (90 . 90)))
+;; (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+;; (add-to-list 'default-frame-alist '(fillscreen . maximized))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/desk/code/sapis")
+    (setq projectile-project-search-path '("~/desk/code/sapis")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
